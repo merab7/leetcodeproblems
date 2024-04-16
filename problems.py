a1189432488fbbbbb9bd8b8603ee84d6ecf7d028
MERAB TODUA <merabtodua7@gmail.com>
2024-04-16 16:09:12 +0200

    palindrom problem

--- problems.py
+++ problems.py
@@ -27,3 +27,24 @@ class Solution:
             if complement in hashmap:
                 return [i, hashmap[complement]]
             hashmap[nums[i]] = i
+
+
+"""Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left."""       
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        matrix = []
+        for y in str(x):
+            matrix.insert(0, y)
+        return "".join(matrix) == str(x)
+ 
